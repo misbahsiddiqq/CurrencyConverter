e5e83c6812264650fbc73d7f47ac4705d84761ad
misbahsiddiqq <misbah.dhanani@gmail.com>
2024-04-06 00:47:45 +0500

    second commit

--- hello.js
+++ hello.js
@@ -30,12 +30,14 @@ let user_answer = await inquirer.prompt([{
         message: 'Enter your amount',
     }
 ]);
-let fromAmount = currency[user_answer.from];
-let toAmount = currency[user_answer.to];
+//dynamic approach / if else
+let fromAmount = currency[user_answer.from]; //exchange rate
+let toAmount = currency[user_answer.to]; // exchange rate
 let amount = user_answer.amount;
-let baseAmount = amount / fromAmount;
+let baseAmount = amount / fromAmount; // formula + based USD amount
 let convertedAmount = baseAmount * toAmount;
+//consoling all together
+// console.log(fromAmount);
+// console.log(toAmount);
+// console.log(amount);
 console.log(convertedAmount);
-console.log(fromAmount);
-console.log(toAmount);
-console.log(amount);
